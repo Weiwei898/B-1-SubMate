7996280821931dfebaedce986db861f25d5caa6b
middle-aged-Weiwei <chinweihsu83221@gmail.com>
2025-09-13 12:43:48 +0800

    feat: product list 初版

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; //中年新增，使用彈出視窗需要
 import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log('Hello world');
